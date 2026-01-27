1540eef88172f202c3af45ce29718dab6f36a924
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-27 17:51:04 -0600

    subsystem stuf

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -18,5 +18,5 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Hopper extends SubsystemBase{
 
-    private final SparkMax m1Hopper = new SparkMax(1, MotorType.kBrushless);
+    private final SparkMax Hopperm1 = new SparkMax(1, MotorType.kBrushless);
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -31,14 +31,20 @@ public class Intake extends SubsystemBase{
   
     private final SparkMax m1 = new SparkMax(1, MotorType.kBrushless);
 
+    private static final double INTAKE_SPEED = 0.75;
+    private static final double OUTTAKE_SPEED = -0.75;
 
+    public void intake() {
+        m1.set(INTAKE_SPEED);
+    }
 
+    public void outtake() {
+        m1.set(OUTTAKE_SPEED);
+    }
 
-
-
-  
-
-
+    public void stop() {
+        m1.stopMotor();
+    }
 
 //----------------------------------------------------------------------------------------------------------------
 
