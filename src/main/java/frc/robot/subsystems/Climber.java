a187bcde0b4a6721d815a52c7524d888b7997cd3
El <ecampus2026@gmail.com>
2026-01-28 16:16:54 -0600

    updated some redundent code

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -14,134 +14,97 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class Climber extends SubsystemBase {
+  private final SparkMax Climbm1 = new SparkMax(1, MotorType.kBrushless);
+  private final SparkMax Climbm2 = new SparkMax(2, MotorType.kBrushless);
 
+  private final RelativeEncoder e1 = Climbm1.getEncoder();
+  private final RelativeEncoder e2 = Climbm1.getEncoder();
 
+  private final double MAX_HEIGHT = 0;
+  private final double MIN_HEIGHT = 0;
+  private final double HOLD_SPEED = 0;
 
+  private final HashMap<Integer, Double> climbHeights = new HashMap<>();
 
-public class Climber extends SubsystemBase{
+  private final double encoderTolerance = 0;
 
-    private final double NULL = 0;
+  private final SparkMaxConfig config = new SparkMaxConfig();
 
-//Dear leader of team 9149 who will be seering this, I'm sorry.
-private final double nulL = NULL;
-private final double nuLl = NULL;
-private final double nuLL = NULL;
-private final double nUll = NULL;
-private final double nUlL = NULL;
-private final double nULl = NULL;
-private final double nULL = NULL;
-private final double Null = NULL;
-private final double NulL = NULL;
-private final double NuLl = NULL;
-private final double NuLL = NULL;
-private final double NUll = NULL;
-private final double NUlL = NULL;
-private final double NULl = NULL;
-
-
-    private final SparkMax Climbm1 = new SparkMax(1, MotorType.kBrushless);
-    private final SparkMax Climbm2 = new SparkMax(2, MotorType.kBrushless);
-
-    private final RelativeEncoder e1 = Climbm1.getEncoder();
-    private final RelativeEncoder e2 = Climbm1.getEncoder();
-
-
-    private final double MAX_HEIGHT = NULL;
-    private final double MIN_HEIGHT = NULL;
-    private final double HOLD_SPEED = NULL;
-
-    private final HashMap<Integer, Double> climbHeights = new HashMap<>();
-    
-    private final double encoderTolerance = NULL;
-
-    
-
-
-    private final SparkMaxConfig config = new SparkMaxConfig();
-
-    public Climber(boolean inverted) {
+  public Climber(boolean inverted) {
     config
-      .inverted(inverted)
-      .idleMode(IdleMode.kBrake);
+        .inverted(inverted)
+        .idleMode(IdleMode.kBrake);
 
     Climbm1.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
     Climbm2.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
     zeroEncoders();
+    initHeights();
   }
 
   private void initHeights() {
-  climbHeights.put(0, NULL); 
-  climbHeights.put(1, NULL);
-  climbHeights.put(2, NULL);
-  
-}
+    climbHeights.put(0, 0.0);
+    climbHeights.put(1, 0.0);
+    climbHeights.put(2, 0.0);
+  }
 
+  public void moveToHeight(double targetHeight, boolean hold) {
+    double error = targetHeight - getHeight();
+    double holdSpeed = hold ? HOLD_SPEED : 0;
 
-public void moveToHeight(double targetHeight, boolean hold) {
-  double error = targetHeight - getHeight();
-  double holdSpeed = hold ? HOLD_SPEED : 0;
+    setSpeed(
+        Math.abs(error) > encoderTolerance
+            ? (error > 0 ? 0.6 : -0.4)
+            : holdSpeed);
+  }
 
-  setSpeed(
-    Math.abs(error) > encoderTolerance
-      ? (error > 0 ? 0.6 : -0.4)
-      : holdSpeed
-  );
-}
+  public void setSpeed(double speed) {
+    Climbm1.set(speed);
+    Climbm2.set(speed);
 
-    public void setSpeed(double speed) {
-        Climbm1.set(speed);
-        Climbm2.set(speed);
+  }
 
-    }
+  public void stop() {
+    Climbm1.set(0);
+    Climbm2.set(0);
 
-    public void stop() {
-        Climbm1.set(0);
-        Climbm2.set(0);
+  }
 
-    }
-
-    public void hold() {
+  public void hold() {
     Climbm1.set(HOLD_SPEED);
     Climbm2.set(HOLD_SPEED);
   }
 
-    public void zeroEncoders() {
+  public void zeroEncoders() {
     e1.setPosition(0);
     e2.setPosition(0);
-    }
+  }
 
-    
-    private boolean withinLimits(double speed) {
-        double height = getHeight();
+  private boolean withinLimits(double speed) {
+    double height = getHeight();
 
-        if (height >= MAX_HEIGHT && speed > 0) return false;
-        if (height <= MIN_HEIGHT && speed < 0) return false;
+    if (height >= MAX_HEIGHT && speed > 0)
+      return false;
+    if (height <= MIN_HEIGHT && speed < 0)
+      return false;
 
-        return true;
-    }
+    return true;
+  }
 
-    public double getHeight() {
-        return (Math.abs(e1.getPosition()) + Math.abs(e2.getPosition())) / 2;
-    }
+  public double getHeight() {
+    return (Math.abs(e1.getPosition()) + Math.abs(e2.getPosition())) / 2;
+  }
 
-    public boolean atHeight(int level) {
-        return Math.abs(getHeight() - climbHeights.get(level)) <= encoderTolerance;
-    }
+  public boolean atHeight(int level) {
+    return Math.abs(getHeight() - climbHeights.get(level)) <= encoderTolerance;
+  }
 
-    public void climbUp() {
-        setSpeed(NULL);
-    }
-
-    public void climbDown() {
-        setSpeed(-NULL);
-    }
-
-    public boolean atMaxHeight() {
+  public boolean atMaxHeight() {
     return getHeight() >= MAX_HEIGHT;
   }
 
-    @Override
+  @Override
   public void periodic() {
     SmartDashboard.putNumber("elevator Encoders", getHeight());
     SmartDashboard.putNumber("encoder one", e1.getPosition());
