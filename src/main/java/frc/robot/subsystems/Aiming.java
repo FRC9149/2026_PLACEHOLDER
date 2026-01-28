d671fd8cd1dc2ec5b7b8163a93065c1aed0b49f1
SomeTrees266 <4estgx@gmail.com>
2026-01-28 17:11:31 -0600

    New Bill, he know about NULL

--- src/main/java/frc/robot/subsystems/Aiming.java
+++ src/main/java/frc/robot/subsystems/Aiming.java
@@ -1,14 +1,5 @@
 package frc.robot.subsystems;
 
-import java.util.HashMap;
-
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
@@ -21,12 +12,9 @@ public class Aiming extends SubsystemBase{
     private final Servo s1OnAiming = new Servo(0);
     private final Servo s2OnAiming = new Servo(1);
 
-    public void setHeight(int height){
+    public void setHeight(double height){
     height = MathUtil.clamp(height, 0, 180);
-
-    
-
-
+    s1OnAiming.setAngle(height);
 
 
     }
