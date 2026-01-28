62ba3e9edd3badcb951c044cffae3c91b90e15c2
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-28 17:35:45 -0600

    Command 4 intake

---
+++ src/main/java/frc/robot/commands/Command_4_intake.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Command_4_intake extends Command{
+  /*
+   Command code for the Intake Subsystem will go here.
+   */
+}
