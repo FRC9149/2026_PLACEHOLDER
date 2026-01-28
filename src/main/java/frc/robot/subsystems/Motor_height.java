c9ddefc010673883bb1abbd4d4318f7154e913e2
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-28 17:15:45 -0600

    I removed warnings to clean up

--- src/main/java/frc/robot/subsystems/Motor_height.java
+++ src/main/java/frc/robot/subsystems/Motor_height.java
@@ -1,17 +1,8 @@
 package frc.robot.subsystems;
-    
-import java.util.HashMap;
 
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase; 
 
 public class Motor_height extends SubsystemBase{
