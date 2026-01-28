3a5f58e11aacb48c60631c767ea54acb6d170a53
El <ecampus2026@gmail.com>
2026-01-28 17:47:23 -0600

    Moved swerve config to constants and added comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,9 @@
 package frc.robot;
 
 import com.robocats.swerve.ModuleConfig;
+import com.robocats.swerve.SwerveConfig;
+import com.robocats.swerve.gyroscope.AhrsGyro;
+import com.studica.frc.AHRS.NavXComType;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -21,10 +24,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
-
   public static class WaypointConstants {
     public static final Pose2d middleShootingPosition = new Pose2d(2.5, 4, new Rotation2d(0 * Math.PI / 180));
     public static final Pose2d leftOfLadderShootingPosition = new Pose2d(2.5, 5.3, new Rotation2d(-30 * Math.PI / 180));
@@ -33,7 +32,6 @@ public final class Constants {
     public static final Pose2d middleOfLadderClimbingPostion = new Pose2d(1.6, 4, new Rotation2d(0 * Math.PI / 180));
     public static final Pose2d rightOfLadderClimbingPosition = new Pose2d(1.6, 3.55, new Rotation2d(0 * Math.PI / 180));
     // x-2.5, y-4 Directly in front of scoring
-    // 
   }
 
   public static final class DriveConstants {
@@ -58,33 +56,42 @@ public final class Constants {
     public static final double kRearRightAbsoluteEncoderOffset = 0.715332; //0.221924;
 
     public static final ModuleConfig moduleConfiguration = new ModuleConfig(
-      kFrontLeftDriveMotorPort,
-      kRearLeftDriveMotorPort,
-      kFrontRightDriveMotorPort,
-      kRearRightDriveMotorPort,
-      kFrontLeftTurningMotorPort,
-      kRearLeftTurningMotorPort,
-      kFrontRightTurningMotorPort,
-      kRearRightTurningMotorPort,
-      kFrontLeftEncoderPort,
-      kRearLeftEncoderPort,
-      kFrontRightEncoderPort,
-      kRearRightEncoderPort,
-      kFrontLeftAbsoluteEncoderOffset,
-      kRearLeftAbsoluteEncoderOffset,
-      kFrontRightAbsoluteEncoderOffset,
-      kRearRightAbsoluteEncoderOffset,
-      //is drive motor reversed
+      kFrontLeftDriveMotorPort, //FL Drive port
+      kRearLeftDriveMotorPort, //BL Drive port
+      kFrontRightDriveMotorPort, //FR Drive port
+      kRearRightDriveMotorPort, //BR Drive port
+      kFrontLeftTurningMotorPort, //FL Turn port
+      kRearLeftTurningMotorPort, //BL Turn port
+      kFrontRightTurningMotorPort, //FR Turn port
+      kRearRightTurningMotorPort, //BR Turn port 
+      kFrontLeftEncoderPort, //FL Encoder port
+      kRearLeftEncoderPort, //BR Encoder port
+      kFrontRightEncoderPort, //FR Encoder port
+      kRearRightEncoderPort, //BR Encoder port
+      kFrontLeftAbsoluteEncoderOffset, //FL Encoder offset
+      kRearLeftAbsoluteEncoderOffset, //BL Encoder offset
+      kFrontRightAbsoluteEncoderOffset, //FR Encoder offset
+      kRearRightAbsoluteEncoderOffset, //BR Encoder offset
+      //is drive motor reversed FL, BL, FR, BR
       false, false, true, true);
+
+    public static final SwerveConfig swerveConfiguration = new SwerveConfig(
+      4 / 20,  // max linear velocity
+      3 * Math.PI / 10, // max angular velocity
+      .1016, //wheel diameter
+      TimedRobot.kDefaultPeriod, 
+      DriveConstants.kDriveKinematics, 
+      DriveConstants.moduleConfiguration, 
+      new AhrsGyro(NavXComType.kUSB1, Math.PI/2, false),
+      true //is field symmetric
+    );
       
 
     // If you call DriveSubsystem.drive() with a different period make sure to update this.
     public static final double kDrivePeriod = TimedRobot.kDefaultPeriod;
 
-    public static final double kTrackWidth = 0.56515;
-    // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = 0.56515 ;
-    // Distance between front and back wheels on robot
+    public static final double kTrackWidth = 0.56515; // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = 0.56515 ; // Distance between front and back wheels on robot
 
     public static final SwerveDriveKinematics kDriveKinematics =
         new SwerveDriveKinematics(
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,23 +5,13 @@
 package frc.robot;
 
 import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.Aiming;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-import com.robocats.swerve.SwerveConfig;
 import com.robocats.swerve.SwerveSubsystem;
-import com.studica.frc.AHRS.NavXComType;
-
-
-import com.robocats.swerve.gyroscope.AhrsGyro;
-import com.robocats.swerve.ModuleConfig;
 import com.robocats.controllers.Ps3;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -29,15 +19,14 @@ import com.robocats.controllers.Ps3;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-import com.robocats.controllers.RevGamePad;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final SwerveSubsystem Swerve = new SwerveSubsystem(new SwerveConfig(4, 3 * Math.PI, .1016, TimedRobot.kDefaultPeriod, 
-    DriveConstants.kDriveKinematics, 
-    DriveConstants.moduleConfiguration, 
-    new AhrsGyro(NavXComType.kUSB1, Math.PI/2, false),
-    true
-  ), new PIDController(0.5,0.01,0.01), null, true);
+  private final SwerveSubsystem Swerve = new SwerveSubsystem(
+    DriveConstants.swerveConfiguration, 
+    new PIDController(0.5,0.01,0.01), 
+    null,
+     true
+  );
 
 
     private Ps3 ps3Controller = new Ps3(0);
@@ -50,9 +39,9 @@ public class RobotContainer {
 
     Swerve.setDefaultCommand(
       new RunCommand(() -> Swerve.drive(
-        ps3Controller.getLeftY()/20,
-        ps3Controller.getLeftX()/20, 
-        ps3Controller.getRightX() / 10,
+        ps3Controller.getLeftY(),
+        ps3Controller.getLeftX(), 
+        ps3Controller.getRightX(),
         true
         ), Swerve)
     );
