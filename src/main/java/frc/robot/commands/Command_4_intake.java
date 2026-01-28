8fb3105fccd1daf976a1afec770e2c057a8115b4
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-28 17:37:18 -0600

    fixed it up a little

--- src/main/java/frc/robot/commands/Command_4_intake.java
+++ src/main/java/frc/robot/commands/Command_4_intake.java
@@ -1,9 +1,5 @@
 package frc.robot.commands;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Command_4_intake extends Command{
