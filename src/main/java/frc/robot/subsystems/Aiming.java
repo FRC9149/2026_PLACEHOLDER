15b9c3fa3615497708562490b8b8ab2bbda5a132
ヒュゴ <hnelson.2029@students.rfsd.k12.wi.us>
2026-01-28 18:31:41 -0600

    stsujrfghigkbj

--- src/main/java/frc/robot/subsystems/Aiming.java
+++ src/main/java/frc/robot/subsystems/Aiming.java
@@ -2,19 +2,18 @@ package frc.robot.subsystems;
 
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Aiming extends SubsystemBase{
 
     private final Servo s1OnAiming = new Servo(0);
-    private final Servo s2OnAiming = new Servo(1);
+    private final Servo s2OnAiming = new Servo(0);
 
     public void setHeight(double height){
     height = MathUtil.clamp(height, 0, 180);
     s1OnAiming.setAngle(height);
+    s2OnAiming.setAngle(height);
 
 
     }
